bbabd7777d2ef2815ecb89a7cd492345b0b0c352
이보연 <btothey99@ewhain.net>
2024-05-18 00:30:00 +0900

    feat: 마이페이지 동네 인증 기능 구현

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,41 +1,42 @@
 import axios from 'axios'
-import { useState, useEffect } from "react"
-const { VITE_REACT_API_URL } = import.meta.env;
+import { useState, useEffect } from 'react'
+import { MyPositionSetting } from './components'
+
+const { VITE_REACT_API_URL } = import.meta.env
 
 function MyPage() {
-  const [userInfo, setUserInfo] = useState(null);
-  const [error, setError] = useState('');
-  const [boards, setBoards] = useState([])
+  // const [userInfo, setUserInfo] = useState(null)
+  const [error, setError] = useState('')
+  const [step, setStep] = useState(1)
 
-  // 테스트용
-  // const [userInfo, setUserInfo] = useState({
-  //   user_id: 2,
-  //   email: "test@test.com",
-  //   nickname: "테스트 ",
-  // })
+  // 테스트용 추후 삭제
+  const [userInfo, setUserInfo] = useState({
+    user_id: 2,
+    email: 'test@test.com',
+    nickname: '테스트 ',
+  })
 
-  useEffect(() => {
-    const getUserInfo = async () => {
-      try {
-        // 현재 로그인 중이라면
-        // 토큰 만료 확인 추가
-        // 서버에 GET 요청을 보내고, 현재 로그인 중인 회원의 정보를 받아옴
-        // const response = await axios.get(VITE_REACT_API_URL + `user/mypage/${userInfo.userId}`);
-        const response = await axios.get(VITE_REACT_API_URL + `user/mypage/2`);
-        setUserInfo(response.data.data);
-        console.log(response.status)
-        
-        // 로그인 중이 아니라면
-        // 로그인 페이지로 이동
-  
-      } catch (error) {
-        setError('회원 정보를 가져오는데 실패하였습니다.');
-        console.error('회원 정보 가져오기 오류:', error);
-      }
-    }
-    
-    getUserInfo()
-  }, []);
+  // useEffect(() => {
+  //   const getUserInfo = async () => {
+  //     try {
+  //       // 현재 로그인 중이라면
+  //       // 토큰 만료 확인 추가
+  //       // 서버에 GET 요청을 보내고, 현재 로그인 중인 회원의 정보를 받아옴
+  //       // const response = await axios.get(VITE_REACT_API_URL + `user/mypage/${userInfo.userId}`);
+  //       const response = await axios.get(VITE_REACT_API_URL + `/user/mypage/1`)
+  //       setUserInfo(response.data.data)
+  //       console.log(response.status)
+
+  //       // 로그인 중이 아니라면
+  //       // 로그인 페이지로 이동
+  //     } catch (error) {
+  //       setError('회원 정보를 가져오는데 실패하였습니다.')
+  //       console.error('회원 정보 가져오기 오류:', error)
+  //     }
+  //   }
+
+  //   getUserInfo()
+  // }, [])
 
   if (!userInfo) {
     return (
@@ -51,31 +52,36 @@ function MyPage() {
       setUserInfo(null)
       //sessionStorage.removeItem("accessToken")
       //sessionStorage.removeItem("refreshToken")
-    } catch(error) {
+    } catch (error) {
       console.error(error)
     }
-  };
+  }
 
-  const handleFetchPosts = async () => {
+  const handleLocalAuth = async () => {
     try {
-      // 서버에 사용자가 작성한 게시물 목록을 가져오는 요청을 보냄
-      const response = await axios.get(VITE_REACT_API_URL + `post/user/${userInfo.user_id}`);
-      setPosts(response.data);
+      // 동네 인증 이동
+      setStep(2)
     } catch (error) {
-      setError('게시물을 가져오는데 실패하였습니다.');
-      console.error('게시물 가져오기 오류:', error);
+      setError('동네 인증 가져오는데 실패하였습니다.')
+      console.error('동네 인증 가져오기 오류:', error)
     }
-  };
+  }
 
   return (
-    <div className="flex items-center h-screen w-full justify-center">
-      <div className="w-64">
-        <div className="bg-white shadow-xl rounded-lg py-3">
+    <div className="flex flex-col justify-center items-center h-full w-full bg-white shadow-xl rounded-lg py-3">
+      {step === 1 ? (
+        <div>
           <div className="photo-wrapper p-2">
-            <img className="w-32 h-32 rounded-full mx-auto" src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp" alt="profile"/>
+            <img
+              className="w-32 h-32 rounded-full mx-auto"
+              src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp"
+              alt="profile"
+            />
           </div>
           <div className="p-2">
-            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">{userInfo.nickname}</h3>
+            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+              {userInfo.nickname}
+            </h3>
             <div className="text-center text-gray-400 text-xs font-semibold">
               <p>@{userInfo.user_id}</p>
             </div>
@@ -85,27 +91,39 @@ function MyPage() {
                   <td className="px-2 py-2 text-gray-500 font-semibold">Email</td>
                   <td className="px-2 py-2">{userInfo.email}</td>
                 </tr>
-            </tbody></table>
+              </tbody>
+            </table>
 
             <div className="text-center my-3">
-                <a className="text-xs text-indigo-500 hover:text-indigo-600 font-medium m-2" href="#">회원정보 수정</a>
-                <a className="text-xs text-indigo-500 hover:text-indigo-600 font-medium" href="#" onClick={handleFetchPosts}>게시글 조회</a>
+              <p>내가 받은 포인트</p>
+              <p>내가 준 포인트</p>
             </div>
-            <div>
-              {/* 라우터 이용해서 view 변환 */}
-              <ul>
-                {boards.map(board => (
-                  <li key={board.id}>{board.title}</li>
-                ))}
-              </ul>
-            </div> 
-            <div className="flex justify-center items-center text-center">
-              <button className="middle none center rounded-lg bg-indigo-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-indigo-500/20 transition-all hover:shadow-lg hover:shadow-indigo-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" onClick={handleLogout}>로그아웃</button>
 
+            <div className="text-center my-3">
+              <a className="text-xs text-indigo-500 hover:text-indigo-600 font-medium m-2" href="#">
+                회원정보 수정
+              </a>
+              <a
+                className="text-xs text-indigo-500 hover:text-indigo-600 font-medium"
+                href="#"
+                onClick={handleLocalAuth}
+              >
+                새로운 동네 인증
+              </a>
             </div>
+            <div className="flex justify-center items-center text-center">
+              <button
+                className="middle none center rounded-lg bg-indigo-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-indigo-500/20 transition-all hover:shadow-lg hover:shadow-indigo-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                onClick={handleLogout}
+              >
+                로그아웃
+              </button>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
+      ) : (
+        <MyPositionSetting setStep={setStep} />
+      )}
     </div>
   )
 }
