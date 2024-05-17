0ead6449380b52c21cd7051b795b153a9b58e1f0
hayamaster <dnfxmfkdbzos@naver.com>
2024-05-18 02:55:46 +0900

    feat: 로고 추가

--- src/assets/Icons/index.js
+++ src/assets/Icons/index.js
@@ -1,2 +1,4 @@
 export { default as VisibleEyeIcon } from './visible_eye.svg?react'
 export { default as InvisibleEyeIcon } from './invisible_eye.svg?react'
+export { default as LogoIcon } from './logo.svg?react'
+export { default as LogoSmallIcon } from './logo_small.svg?react'
