3e5df23a60e81885b2ab83727728a778e6280aa1
김혜주 <sueelly@naver.com>
2024-05-17 12:59:05 +0900

    [feat] posting, contentsList page 파일 생성, 높이 조절되는 custom textarea

--- src/pages/posting/Posting.jsx
+++ src/pages/posting/Posting.jsx
@@ -1,3 +1,30 @@
+import { useState } from 'react'
+import TextArea from '../../components/customTextArea'
+
 export default function Posting() {
-  return <div>posting</div>
+  const [title, setTitle] = useState('')
+  const [text, setText] = useState('')
+
+  return (
+    <div>
+      <TextArea
+        placeholder="제목"
+        value={title}
+        onChange={(e) => {
+          e.stopPropagation()
+          setTitle(e.currentTarget.value)
+        }}
+      />
+      <TextArea
+        placeholder="나만의 글쓰기를 시작해보세요."
+        id="editorContents"
+        value={text}
+        onChange={(e) => {
+          e.stopPropagation()
+          setText(e.currentTarget.value)
+        }}
+      />
+      <label htmlFor="editorContents" className="editorEmptySection" />
+    </div>
+  )
 }
