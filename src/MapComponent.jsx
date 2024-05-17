88ebdb003292069488fa75fa60fbbba3657fd677
이보연 <btothey99@ewhain.net>
2024-05-17 11:33:05 +0900

    feat: 사용자 컨트롤과 마커 구현

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -1,23 +1,33 @@
+/*global kakao*/ 
+import { useEffect, useState } from "react"
 import { useKakaoLoader } from "react-kakao-maps-sdk"
-import { Map, MapMarker, CustomOverlayMap } from "react-kakao-maps-sdk"
+import { Map, MapMarker, MapTypeControl, ZoomControl, MarkerClusterer, CustomOverlayMap } from "react-kakao-maps-sdk"
 
 // https://www.npmjs.com/package/react-kakao-maps-sdk
 // https://react-kakao-maps-sdk.jaeseokim.dev/docs/sample
+
 function MapComponent() {
-  const options = {}
-  const [loading, error] = useKakaoLoader({
-    appkey: import.meta.env.VITE_KAKAO_MAP_API_KEY,
-    ...options,
+  useKakaoLoader()
+  const [state, setState] = useState({
+    // 지도의 초기 위치
+    center: { lat: 33.450701, lng: 126.570667 },
+    // 지도 위치 변경시 panto를 이용할지에 대해서 정의
+    isPanto: true,
   })
+  
   return (
     <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
+      center={state.center}
+      isPanto={state.isPanto}
       style={{ width: "100%", height: "360px" }}
+      level={3}
     >
-        
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+      <MapMarker position={state.center}>
         <div style={{color:"#000"}}>Hello World!</div>
       </MapMarker>
+
+      <MapTypeControl position={"TOPRIGHT"} />
+        <ZoomControl position={"RIGHT"} />
     </Map>
   )
 }
