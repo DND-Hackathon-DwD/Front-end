0496817d177281f9cb4e822ddf3510c3e47ab2de
김혜주 <sueelly@naver.com>
2024-05-18 07:19:49 +0900

    [feat] 띱! 버튼 나오게

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import UserContextProvider from './context/userContext'
+import { DDip } from './assets/Icons';
+import { useState } from 'react';
 
 function App() {
+  const [circles, setCircles] = useState([]);
+
+  const handleCanvasClick = (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    // 새로운 동그라미와 각도를 추가합니다.
+    setCircles([...circles, { x, y }]);
+
+    // 0.5초 후에 동그라미를 제거합니다.
+    setTimeout(() => {
+      setCircles(circles.filter((circle) => circle.x !== x || circle.y !== y));
+    }, 270);
+  };
+
   return (
-    <main className="h-dvh flex overflow-hidden relative">
+    <main className="h-dvh flex overflow-hidden relative" onClick={handleCanvasClick}>
+      {circles.map((circle, index) => (
+        <div
+          key={index}
+          style={{
+            zIndex: 100,
+            position: 'absolute',
+            left: circle.x - 10,
+            top: circle.y - 10,
+            objectFit: 'contain',
+            transform: `rotate(${index.rotation * 40}deg)`
+          }} >
+          <DDip />
+        </div>))}
       <UserContextProvider>
         <RouterProvider router={router} />
       </UserContextProvider>
