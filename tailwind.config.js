b2e8796be6608260df16587b4d59be4285ba533e
hayamaster <dnfxmfkdbzos@naver.com>
2024-05-18 05:00:50 +0900

    feat: 메인페이지 마크업 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
+    colors: {
+      primary: '#FF9135',
+      gray: {
+        400: '#A3A3A3',
+      },
+    },
     extend: {},
   },
   plugins: [],
