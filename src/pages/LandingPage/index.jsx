828536ea7cdb122e1e6a32a00a537124b68c5f98
hayamaster <dnfxmfkdbzos@naver.com>
2024-05-18 10:18:17 +0900

    feat: 랜딩페이지 애니메이션 적용

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,15 +1,30 @@
 import { LogoIcon, LogoSmallIcon } from '@/assets/Icons'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const LandingPage = () => {
   const navigate = useNavigate()
+
+  const [isAnimating, setIsAnimating] = useState(false)
+
+  const handleButtonClick = (url) => {
+    setIsAnimating(true)
+    setTimeout(() => {
+      setIsAnimating(false)
+      ;(() => navigate(url))()
+    }, 2000)
+  }
+
   return (
     <div className="w-full h-full flex flex-col items-center">
       <i className="w-full h-full flex justify-center items-center">
-        <LogoIcon className="w-48 h-48" />
+        <LogoIcon className="w-48 h-48" />.
       </i>
       <div className="flex flex-col gap-6 px-6 py-8 w-full">
-        <button className="bg-primary w-full rounded-lg py-4" onClick={() => navigate('/login')}>
+        <button
+          className="bg-primary w-full rounded-lg py-4"
+          onClick={() => handleButtonClick('/login')}
+        >
           <div className="flex justify-center items-center gap-1">
             <LogoSmallIcon className="w-10 fill-[#FFF]" />
             <p className="text-[#fff]">로그인하기</p>
@@ -17,7 +32,7 @@ const LandingPage = () => {
         </button>
         <button
           className="bg-[#FFF7ED] py-4 border border-primary w-full rounded-lg"
-          onClick={() => navigate('/signup')}
+          onClick={() => handleButtonClick('/signup')}
         >
           <div className="flex justify-center items-center gap-1">
             <p className="text-gray-400">지금 바로</p>
@@ -25,6 +40,22 @@ const LandingPage = () => {
             <p className="text-gray-400">회원가입하기</p>
           </div>
         </button>
+      </div>
+
+      <div
+        className={`flex justify-center items-center ${
+          isAnimating ? 'animate-backgroundFadeIn z-50 fixed inset-0 ' : 'animate-backgroundFadeOut'
+        }`}
+      >
+        {isAnimating && (
+          <div className="absolute inset-0 flex justify-center items-center">
+            <LogoIcon
+              className={`animate-rotate w-48 h-48 fill-[#FFFF] ${
+                isAnimating ? '' : 'animate-fadeOut'
+              }`}
+            />
+          </div>
+        )}
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,44 @@ export default {
         400: '#A3A3A3',
       },
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        fadeOut: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+        backgroundFadeIn: {
+          '0%': { backgroundColor: 'rgba(0, 0, 0, 0)' },
+          '100%': { backgroundColor: 'rgba(255, 165, 0, 0.8)' },
+        },
+        backgroundFadeOut: {
+          '0%': { backgroundColor: 'rgba(255, 165, 0, 0.8)' },
+          '100%': { backgroundColor: 'rgba(0, 0, 0, 0)' },
+        },
+        scale: {
+          '0%': { transform: 'scale(0)' },
+          '50%': { transform: 'scale(1.2)' },
+          '100%': { transform: 'scale(1)' },
+        },
+        rotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(180deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in-out',
+        fadeOut: 'fadeOut 2s ease-in-out',
+        backgroundFadeIn: 'backgroundFadeIn 2s ease-in-out',
+        // backgroundFadeOut: 'backgroundFadeOut 2s ease-in-out',
+        scale: 'scale 1s ease-in-out',
+        rotate: 'rotate 0.5s ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
